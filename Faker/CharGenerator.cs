a93a1a4e3811485872f4df8f482e8421eaf1b6fc
Constantin Hitov <kostya.khitov@mail.ru>
2019-10-30 19:08:30 +0300

    Implement simple generators

---
+++ Faker/CharGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IGeneratorLibrary;
+
+namespace Faker
+{
+    public class CharGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            Random rnd = new Random();
+            return (char)rnd.Next(0, 256);
+        }
+
+        public Type GeneratedType()
+        {
+            return typeof(char);
+        }
+    }
+}
