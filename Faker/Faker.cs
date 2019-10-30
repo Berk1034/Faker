100d0d733b0f8da38dbd6057f83605e96ac22ffd
Constantin Hitov <kostya.khitov@mail.ru>
2019-10-30 19:08:57 +0300

    Implement ListGenerator

--- Faker/Faker.cs
+++ Faker/Faker.cs
@@ -13,22 +13,49 @@ namespace Faker
     {
         public Dictionary<Type, IGenerator> Generators = new Dictionary<Type, IGenerator>();
         public Stack<Type> generationStack;
+        public ListGenerator listGenerator;
 
         public Faker()
         {
-            List<Assembly> Plugins = Plugin.LoadPlugin(Path.GetDirectoryName(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName) + Plugin.path + "ByteGenerator.dll");
+            List<Assembly> Plugins = Plugin.LoadPlugin(Path.GetDirectoryName(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName) + Plugin.path + "ByteGenerator.dll", Path.GetDirectoryName(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName) + Plugin.path + "FloatGenerator.dll");
             Generators = Plugin.GetGenerators(Plugins);
             Generators.Add(typeof(int), new IntGenerator());
             Generators.Add(typeof(bool), new BoolGenerator());
+            Generators.Add(typeof(DateTime), new DateTimeGenerator());
+            Generators.Add(typeof(char), new CharGenerator());
+            Generators.Add(typeof(double), new DoubleGenerator());
+            Generators.Add(typeof(long), new LongGenerator());
+            Generators.Add(typeof(string), new StringGenerator());
+            listGenerator = new ListGenerator(Generators);
         }
 
         public T Create<T>()
         {
+            Type type = typeof(T);
+            if(type.IsAbstract || type.IsInterface || type == typeof(void))
+            {
+                return default(T);
+            }
+            if (type.IsGenericType)
+            {
+                return (T)listGenerator.Generate((Type)type.GenericTypeArguments.GetValue(0));
+            }
+            IGenerator value;
+            Generators.TryGetValue(type, out value);
+            if(value!= null)
+            {
+                return (T)value.Generate();
+            }
+            else
+            {
+                return default(T);
+            }
+            /*
             foreach(KeyValuePair<Type, IGenerator> generator in Generators){
                 if (generator.Key == typeof(T))
                     return (T)generator.Value.Generate();
             }
-            return default(T);
+            */
         }
     }
 }
--- Faker/Program.cs
+++ Faker/Program.cs
@@ -14,7 +14,20 @@ namespace Faker
             int t = faker.Create<int>();
             bool b = faker.Create<bool>();
             byte c = faker.Create<byte>();
-            Console.WriteLine("{0}, {1}, {2}", t, b, c);
+            float f = faker.Create<float>();
+            DateTime dt = faker.Create<DateTime>();
+            TimeSpan TS = faker.Create<TimeSpan>();
+            char ch = faker.Create<char>();
+            double d = faker.Create<double>();
+            long lng = faker.Create<long>();
+            string str = faker.Create<string>();
+            List<int> listtest = faker.Create<List<int>>();
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}", t, b, c, f, dt, TS, ch, d, lng, str);
+            Console.WriteLine();
+            for(int i = 0; i < listtest.Count(); i++)
+            {
+                Console.WriteLine("{0}", listtest.ElementAt(i));
+            }
             Console.ReadLine();
         }
     }
