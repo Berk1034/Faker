52aa641de3d3b61cd77bc48283f3f5fc629ecb40
Constantin Hitov <kostya.khitov@mail.ru>
2019-10-30 12:02:51 +0300

    Implement ByteGenerator and move it to dll

---
+++ ByteGenerator/ByteGenerator/ByteGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IGeneratorLibrary;
+
+namespace ByteGenerator
+{
+    public class ByteGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            Random rnd = new Random();
+            return (byte)rnd.Next(1, byte.MaxValue);
+        }
+
+        public Type GeneratedType()
+        {
+            return typeof(byte);
+        }
+    }
+}
