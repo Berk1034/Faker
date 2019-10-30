9daec173184a5a97be9bf19012e2183ad6ba8e64
Constantin Hitov <kostya.khitov@mail.ru>
2019-10-30 11:58:58 +0300

    Update IGenerator structure and move it to dll

--- Faker/BoolGenerator.cs
+++ Faker/BoolGenerator.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IGeneratorLibrary;
 
 namespace Faker
 {
-    public class BoolGenerator : IGenerator<bool>
+    public class BoolGenerator : IGenerator
     {
-        public bool Generate()
+        public Type GeneratedType()
+        {
+            return typeof(bool);
+        }
+
+        public object Generate()
         {
             return true; //RETURN NOT 0 VALUES!   
         }
--- IGeneratorLibrary/IGeneratorLibrary/IGenerator.cs
+++ IGeneratorLibrary/IGeneratorLibrary/IGenerator.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Faker
+namespace IGeneratorLibrary
 {
-    public interface IGenerator<T>
+    public interface IGenerator
     {
-        T Generate();
+        object Generate();
+        Type GeneratedType();
     }
 }
