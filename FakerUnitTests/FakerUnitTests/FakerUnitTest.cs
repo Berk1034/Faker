26061b8c801e65c05262c45cc28ef26d990954d2
Constantin Hitov <kostya.khitov@mail.ru>
2019-11-06 01:42:08 +0300

    Add FakerUnitTests with basic test

---
+++ FakerUnitTests/FakerUnitTests/FakerUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakerLibrary;
+using System.Collections.Generic;
+
+namespace FakerUnitTests
+{
+    [TestClass]
+    public class FakerUnitTest
+    {
+        private readonly Faker faker = new Faker();
+        [TestMethod]
+        public void ShouldFillListWithDifferentValues()
+        {
+            List<int> list = faker.Create<List<int>>();
+            Assert.IsNotNull(list);
+        }
+    }
+}
---
+++ FakerUnitTests/FakerUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FakerUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakerUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dd335b5d-b875-4aa0-b116-38627089e67d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
